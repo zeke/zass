9679b297dbf67e64f376f697947bf69df99438bb
zeke <zeke@sikelianos.com>
2011-04-19 13:54:53 -0700

    Added some mixins and filled out the project manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,2 +1,23 @@
 # Make sure you list all the project template files here in the manifest.
-stylesheet 'screen.sass', :media => 'screen, projection'
+# http://compass-style.org/docs/tutorials/extensions/
+
+description "Zass: a collection of sass mixins."
+
+discover :all
+
+# This is a message that users will see if they type
+#   compass help my_extension
+help %Q{
+To use zass mixins in your project, add the following line to your main sass file:
+  
+@import "zass"
+
+}
+
+welcome_message %Q{
+To use zass mixins in your project, add the following line to your main sass file:
+
+@import "zass"
+
+}
+
